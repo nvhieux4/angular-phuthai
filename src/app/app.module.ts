c44e34381bb236a4b0fb970f66132acda381c851
Nguyễn Văn Hiếu <nvhieux4@gmail.com>
2021-10-14 15:48:43 +0700

    add code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './component/Footer/footer.component';
 import { HeaderComponent } from './component/header/header.component';
 import { ListProductComponent } from './component/list-product/list-product.component';
+import { NewsComponent } from './component/news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListProductComponent
+    ListProductComponent,
+    NewsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
